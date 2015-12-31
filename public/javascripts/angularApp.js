e5029b2c363c39f3d07a2780c93c56d89ffd3b80
Thomas Cho <s.thomas.cho@gmail.com>
2015-12-30 17:52:35 -0700

    posts ordered by upvotes

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -4,4 +4,11 @@ app.controller('MainController', [
 '$scope',
 function($scope){
   $scope.test = 'Hello world!';
+  $scope.posts = [
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 3},
+		{title: 'post 3', upvotes: 7},
+		{title: 'post 4', upvotes: 8},
+		{title: 'post 5', upvotes: 4}
+  ]
 }]);
