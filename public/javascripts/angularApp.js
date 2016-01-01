8579c88378f0c7795e143467c5d85911f4e878a5
Thomas Cho <s.thomas.cho@gmail.com>
2016-01-01 14:38:37 -0700

    Added styling with bootstrap

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -3,12 +3,27 @@ var app = angular.module('courseNews', []);
 app.controller('MainController', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  
   $scope.posts = [
 		{title: 'post 1', upvotes: 5},
 		{title: 'post 2', upvotes: 3},
 		{title: 'post 3', upvotes: 7},
 		{title: 'post 4', upvotes: 8},
 		{title: 'post 5', upvotes: 4}
-  ]
+  ];
+	
+	$scope.addPost = function() {
+		if(!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({
+			title: $scope.title, 
+			link: $scope.link,
+			upvotes: 0
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
 }]);
