017637bd01aa344dcc004566da46471ce9e3deff
Thomas Cho <s.thomas.cho@gmail.com>
2016-01-03 11:55:23 -0700

    Made angular service for posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,16 +1,20 @@
 var app = angular.module('courseNews', []);
 
-app.controller('MainController', [
-'$scope',
-function($scope){
-  
-  $scope.posts = [
+app.factory('posts', [function(){
+	var object = {
+		posts: [
 		{title: 'post 1', upvotes: 5},
 		{title: 'post 2', upvotes: 3},
 		{title: 'post 3', upvotes: 7},
 		{title: 'post 4', upvotes: 8},
 		{title: 'post 5', upvotes: 4}
-  ];
+  ]
+	};
+	return object;
+}])
+
+app.controller('MainController', ['$scope', 'posts', function($scope, posts){
+    $scope.posts = posts.posts;
 	
 	$scope.addPost = function() {
 		if(!$scope.title || $scope.title === '') { return; }
